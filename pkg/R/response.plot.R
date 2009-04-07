07ced779b0fdea6a848113e22c9e22264fd20db8
brunolafourcade (bruno lafourcade) <funkycastor@aol.com>
2009-04-07 09:47:35 +0000

    re-generalised the function for any kind of model. No further direct link to Biomod.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biomod@24 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/response.plot.R
+++ pkg/R/response.plot.R
@@ -1,56 +1,49 @@
 `response.plot` <-
-function(mod, Sp){
+function(model, Data){
 
-    Data <- DataBIOMOD[,1:Biomod.material[["NbVar"]]]
-    Xp <- as.data.frame(matrix(sapply(Data, mean), nrow(Data), Biomod.material[["NbVar"]], byrow=TRUE, dimnames=list(NULL, colnames(Data))))
-    if(!Biomod.material[["algo.choice"]][mod]) stop("The selected model has not been run")
-    if(Sp>length(Biomod.material[["species.names"]])) stop(paste("Wrong species index : there was only", length(Biomod.material[["species.names"]]), "species modelled", sep=" ")) 
-    
-    object <- eval(parse(text=load(paste(getwd(), "/models/", Biomod.material[["species.names"]][Sp], "_", mod, sep=""))))
+    NbVar <- ncol(Data)
+    Xp <- as.data.frame(matrix(sapply(Data, mean), nrow(Data), NbVar, byrow=TRUE, dimnames=list(NULL, colnames(Data))))
        
-    if(mod=="ANN") if(sum(search()=="package:nnet")==0) library(nnet)
-    if(mod=="CTA") if(sum(search()=="package:rpart")==0) library(rpart)
-    if(mod=="MARS" | mod=="MDA") if(sum(search()=="package:mda")==0) library(mda)
-    if(mod=="RF") if(sum(search()=="package:randomForest")==0) library(randomForest,  verbose=F)
+    if(class(model)[1]=="nnet.formula") if(sum(search()=="package:nnet")==0) library(nnet)
+    if(class(model)[1]=="rpart") if(sum(search()=="package:rpart")==0) library(rpart)
+    if(class(model)[1]=="mars" | class(model)[1]=="mda") if(sum(search()=="package:mda")==0) library(mda)
+    if(class(model)[1]=="randomForest") if(sum(search()=="package:randomForest")==0) library(randomForest,  verbose=F)
     
     x11()
-    if(!is.na(VarImportance)[[1]]) sqnb <- ceiling(sqrt(Biomod.material[["NbVar"]]+1)) else sqnb <- ceiling(sqrt(Biomod.material[["NbVar"]]))
-    layout(matrix(c(rep(1,sqnb),2:(sqnb^2+1)),nc=sqnb, byrow=T), widths=rep(1,sqnb), heights=c(0.3,rep(1,ceiling(Biomod.material[["NbVar"]]/sqnb))))
+    sqnb <- ceiling(sqrt(NbVar))   #if(!is.na(VarImportance)[[1]]) sqnb <- ceiling(sqrt(NbVar+1)) else  sqnb <- ceiling(sqrt(NbVar))
+    layout(matrix(c(rep(1,sqnb),2:(sqnb^2+1)),nc=sqnb, byrow=T), widths=rep(1,sqnb), heights=c(0.3,rep(1,ceiling(NbVar/sqnb))))
     
     par(mar = c(0.1, 0.1, 0.1, 0.1))
     plot(x=c(-1,1),y=c(0,1),xlim=c(0,1),ylim=c(0,1),type="n",axes=FALSE)
     polygon(x=c(-2,-2,2,2),y=c(-2,2,2,-2),col="#f5fcba",border=NA)
-    text(x=0.5,y=0.8,pos=1,cex=1.6,labels=paste("Response curves for species", Sp, "  by  ",mod,sep=""),col="#4c57eb")
+    text(x=0.5, y=0.8, pos=1, cex=1.6, labels=paste("Response curves", class(model)[1], sep=""),col="#4c57eb")
     par(mar = c(2,2,3.5,1))
 
-    for(i in 1:Biomod.material[["NbVar"]]) {
+    for(i in 1:NbVar) {
     
         xr <- sapply(Data, range)
         Xp1 <- Xp
         Xp1[,i] <- seq(xr[1,i], xr[2,i],  len=nrow(Data))
 
-        if(mod=="GLM" | mod=="GAM") Xf <- predict(object, as.data.frame(Xp1), type="response")
-        #if(mod=="GBM") Xf <-  predict.gbm(object, as.data.frame(Xp1), object$n.trees, type='response')
-        if(mod=="GBM") Xf <-  predict.gbm(object, as.data.frame(Xp1), Models.information[[Sp]]$GBM$best.iter, type="response")
-        if(mod=="CTA") Xf <- as.numeric(predict(object, Xp1, type="vector"))
-        if(mod=="ANN") Xf <- Rescaler2(as.numeric(predict(object, as.data.frame(Xp1), type="raw")), OriMinMax=Models.information[[Sp]]$ANN$RawPred)
-        if(mod=="MARS") Xf <- Rescaler2(as.numeric(predict(object, as.data.frame(Xp1))), OriMinMax=Models.information[[Sp]]$MARS$RawPred)
-        if(mod=="MDA") Xf <- Rescaler2(predict(object, as.data.frame(Xp1), type="post")[,2], OriMinMax=Models.information[[Sp]]$MDA$RawPred)
-        if(mod=="RF") Xf <- Rescaler2(predict(object, as.data.frame(Xp1), type="prob")[,2], OriMinMax=Models.information[[Sp]]$RF$RawPred)
+        if(class(model)[1]=="glm" | class(model)[1]=="gam") Xf <- predict(model, as.data.frame(Xp1), type="response")
+        if(class(model)[1]=="gbm") Xf <-  predict.gbm(model, as.data.frame(Xp1), model$n.trees, type="response") #if(mod=="GBM") Xf <-  predict.gbm(model, as.data.frame(Xp1), model$n.trees, type='response')
+        if(class(model)[1]=="rpart") Xf <- as.numeric(predict(model, Xp1, type="vector"))
+        if(class(model)[1]=="nnet.formula") Xf <- as.numeric(predict(model, as.data.frame(Xp1), type="raw"))
+        if(class(model)[1]=="mars") Xf <- as.numeric(predict(model, as.data.frame(Xp1)))
+        if(class(model)[1]=="mda") Xf <- predict(model, as.data.frame(Xp1), type="post")[,2]
+        if(class(model)[1]=="randomForest") Xf <- predict(model, as.data.frame(Xp1), type="prob")[,2]
 
-        #if (length(unique(Xf)) !=1)    # pour ne pas afficher les variables ne présenatant aucune variations  ->  non sélectionnées par le modèle
+        if(class(model)[1]=="mars" | class(model)[1]=="nnet.formula" | class(model)[1]=="mda" | class(model)[1]=="randomForest" ) { 
+            Xf[Xf<0]<-0
+            Xf[Xf>1]<-1
+        }
+
         plot(Xp1[ ,i], Xf, ylim=c(0,1), xlab="", ylab="", type="l", main=names(Data)[i])     
     } 
-    if(!is.na(VarImportance)[[1]]){
-        plot(x=c(-1,1),y=c(0,1),xlim=c(0,1),ylim=c(0,1),type="n",axes=FALSE, main="Variable Importance")
-        varimp <- t(VarImportance[[Sp]][mod,])
-        vec <- ""
-        for(i in 1:Biomod.material[["NbVar"]]) vec <- paste(vec, row.names(varimp)[i],"   ", round(varimp[i,1],digits=3), "\n", sep="") 
-        text(x=0.3,y=0.4,pos=4,cex=if(Biomod.material[["NbVar"]]<8){1.2} else {1/(Biomod.material[["NbVar"]]/8)},labels=vec)
-    }    
-    if(mod=="ANN")  detach(package:nnet)
-    if(mod=="CTA") detach(package:rpart)
-    if(mod=="MARS" | mod=="MDA") detach(package:mda)
-    if(mod=="RF") detach(package:randomForest)            
+   
+    if(class(model)[1]=="nnet.formula")  detach(package:nnet)
+    if(class(model)[1]=="rpart") detach(package:rpart)
+    if(class(model)[1]=="mars" | class(model)[1]=="mda") detach(package:mda)
+    if(class(model)[1]=="randomForest") detach(package:randomForest)            
 }
 
