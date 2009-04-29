0d405a8a1090eac04fbafa6ed94f2da1f475435f
brunolafourcade (bruno lafourcade) <funkycastor@aol.com>
2009-04-29 12:59:31 +0000

    not much, still not working
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biomod@47 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ProjectionBestModel.R
+++ pkg/R/ProjectionBestModel.R
@@ -4,23 +4,28 @@ function(Proj.name, Bin.trans=TRUE, Filt.trans=TRUE, method='all')
     Th <- c('Kappa','TSS','Roc','all')
     if(sum(Th == method) == 0) stop("\n : uncorrect method name , should be one of 'Kappa' 'TSS' 'Roc' 'all'")
     
+    #apply the function to the 3 possible transformation methods if all are selected
     if(method == 'all') for(k in 1:3) ProjectionBestModel(Proj.name, Bin.trans, Filt.trans, method=Th[k])
     
-    else { if(Biomod.material[["evaluation.choice"]][method]) {
+    #run the function for one method at a time 
+    else { if(Biomod.material$evaluation.choice[method]) {
     
-        g <- gg <- ggg <-  as.data.frame(matrix(0, nrow=Biomod.material[[paste("proj.", Proj.name, ".length", sep="")]], ncol=Biomod.material[["NbSpecies"]], dimnames= list(seq(Biomod.material[[paste("proj.", Proj.name, ".length", sep="")]]), Biomod.material[["species.names"]])))
+        g <- gg <- ggg <-  as.data.frame(matrix(0, nrow=Biomod.material[[paste("proj.", Proj.name, ".length", sep="")]], ncol=Biomod.material$NbSpecies, dimnames= list(seq(Biomod.material[[paste("proj.", Proj.name, ".length", sep="")]]), Biomod.material$species.names)))
 
         i <- 1
-        while(i <= Biomod.material[["NbSpecies"]]) {
+        while(i <= Biomod.material$NbSpecies) {
             eval(parse(text=paste("load('pred/BestModelBy", method, "')", sep="")))
             j <- as.character(eval(parse(text=paste("BestModelBy",method,"[i,1]",sep=""))))
-            load(paste(getwd(), "/proj.", Proj.name, "/Proj_",Proj.name, "_", Biomod.material[["species.names"]][i],sep=''))
-            g[,i] <- eval(parse(text=paste("Proj",Proj.name,Biomod.material[["species.names"]][i],sep='_')))[,j]
+            load(paste(getwd(), "/proj.", Proj.name, "/Proj_",Proj.name, "_", Biomod.material$species.names[i],sep=''))
+            
+            
+            g[,i] <- eval(parse(text=paste("Proj",Proj.name,Biomod.material$species.names[i],sep='_')))[,j]
             if(Bin.trans)    eval(parse(text=paste("gg[,i] <- BinaryTransformation(g[,i], as.numeric(Evaluation.results.", method, "[[i]][j,4]))",sep='')))
             if(Filt.trans)   eval(parse(text=paste("ggg[,i] <- FilteringTransformation(g[,i], as.numeric(Evaluation.results.", method, "[[i]][j,4]))",sep='')))
             i <- i + 1
         }
-             
+        
+        #objects assignations and storage     
         assign(paste("Proj_",Proj.name,"_BestModelBy",method,sep=""), as.data.frame(g))
         eval(parse(text=paste("save(Proj_",Proj.name,"_BestModelBy",method,", file='", getwd(), "/proj.", Proj.name, "/Proj_",Proj.name,"_BestModelBy",method,"')", sep="")))
             write.table(g, file=paste(getwd(),"/proj.", Proj.name, "/Proj_",Proj.name,"_BestModelBy",method,".txt", sep=""), row.names=F)
