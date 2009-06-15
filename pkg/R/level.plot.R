5052d683db32a6b9dda489d968426fe6c3ff6af1
brunolafourcade (bruno lafourcade) <funkycastor@aol.com>
2009-06-15 15:49:52 +0000

    fixed some problem when assessing colors to values, especially when negative values were involved.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biomod@71 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/level.plot.R
+++ pkg/R/level.plot.R
@@ -5,6 +5,7 @@ function(data.in, XY, color.gradient='red', cex=1, level.range=c(min(data.in),ma
     if(ncol(XY)!=2) stop("\n wrong coordinates given in 'XY' : there should be two columns \n")
     if(nrow(XY)!=length(data.in)) stop("\n data and coordinates should be of the same length \n")
 
+
     if(color.gradient=='grey') {
         color.system <- c()
         for(i in seq(93,10,length.out=100)) color.system <- c(color.system, gray(i/100))
@@ -30,22 +31,35 @@ function(data.in, XY, color.gradient='red', cex=1, level.range=c(min(data.in),ma
       
     #SRCvalues <- c(-2,-1,0,1)
     #if(sum(match(SRCvalues, unique(data.in))) == length(data.in)){ SRC plot}  
-      
-      
+    
+    #if range wanted is broader than possible, set to actual range limits
+    if(level.range[1]<min(data.in)) level.range[1] <- min(data.in)  
+    if(level.range[2]>max(data.in)) level.range[2] <- max(data.in)  
+    
+    #determine the color code to assess to each value  
     g <- gg <- data.in
-    gg <- gg-level.range[1] 
-    gg <- gg/level.range[2] *100+1
-    gg[g<=level.range[1]] <- 1
-    gg[g>=level.range[2]] <- 102
+    gg[gg <= level.range[1]]  <- level.range[1]
+    gg[gg >= level.range[2]] <- level.range[2]
+    gg <- gg-min(g) 
+    gg <- gg/max(gg)*100 + 1
+   
+    #over and under-ranged values set to limits of color range
+    gg[g < level.range[1]] <- 1
+    gg[g > level.range[2]] <- 102
+    
     
     if(show.scale){
+        #setting the values written in legend
+        if(level.range[1] == min(data.in)) lmin <- round(level.range[1], digits=2) else lmin <- paste(round(level.range[1], digits=2), " or lower", sep="")
+        if(level.range[2] == max(data.in)) lmax <- round(level.range[2], digits=2) else {lmax <- paste(round(level.range[2], digits=2), " or over", sep="") ; color.system[102] <- "grey70"}
+    
         layout(matrix(c(1,2),nr=1), widths=c(5,1), heights=c(1,1))
         plot(XY[,2]~XY[,1], col=color.system[gg], cex=cex, pch=19, xlab='', ylab='', xaxt='n', yaxt='n', main=title)
         par(mar=c(0.1,0.1,0.1,0.1))
         plot(x=c(-1,1),y=c(0,1),xlim=c(0,1),ylim=c(0,1),type="n",axes=FALSE) 
         polygon(x=c(-2,-2,2,2),y=c(-2,2,2,-2),col="#f5fcba",border=NA)
-        legend(0.2,0.92,legend=list(round(level.range[2], digits=2),'','','','',round((3*level.range[2]+level.range[1])/4, digits=2),'','','','',round(sum(level.range)/2, digits=2),
-         '','','','',round((level.range[2]+3*level.range[1])/4, digits=2),'','','','',round(level.range[1], digits=2)),cex=1, fill=rev(color.system[c(1,seq(2,101,length.out=19),102)]),bty='n')
+        legend(0.2,0.92,legend=list(lmax,'','','','',round((3*level.range[2]+level.range[1])/4, digits=2),'','','','',round(sum(level.range)/2, digits=2),
+         '','','','',round((level.range[2]+3*level.range[1])/4, digits=2),'','','','',lmin),cex=1, fill=rev(color.system[c(1,seq(2,101,length.out=19),102)]),bty='n')
     }
      else plot(XY[,2]~XY[,1], col=color.system[gg], pch=19, xlab='', ylab='', xaxt='n', yaxt='n', main=title)  
 }
