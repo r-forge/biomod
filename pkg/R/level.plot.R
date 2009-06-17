6ae2634f19d24e94e106f145614cb50d446f73c6
brunolafourcade (bruno lafourcade) <funkycastor@aol.com>
2009-06-17 13:40:17 +0000

    the function can now recognize the SRC values (-2,-1,0,1) and plot them as such.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biomod@73 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/level.plot.R
+++ pkg/R/level.plot.R
@@ -6,61 +6,76 @@ function(data.in, XY, color.gradient='red', cex=1, level.range=c(min(data.in),ma
     if(nrow(XY)!=length(data.in)) stop("\n data and coordinates should be of the same length \n")
 
 
-    if(color.gradient=='grey') {
-        color.system <- c()
-        for(i in seq(93,10,length.out=100)) color.system <- c(color.system, gray(i/100))
-        color.system <- c(gray(0.93), color.system, gray(0))
+    SRC <- F
+    SRCvalues <- c(-2,-1,0,1)
+    if(length(unique(data.in)) <= 4){
+        nb <- 0
+        for(i in 1:length(unique(data.in))) if(sum(unique(data.in)[i]==SRCvalues) == 1) nb <- nb+1
+        if(nb==length(unique(data.in))) SRC <- T
     }
-    if(color.gradient=='blue') {
-        color.system <- c('grey88',
-        rainbow(45, start=0.5, end=0.65),                       
-        rainbow(10, start=0.65, end=0.7),
-        rainbow(45, start=0.7, end=0.85),
-        'red')
-    }
-    if(color.gradient=='red') {    
-        color.system <- c(
-        'grey88',
-        c(rep(c(colors()[c(417,417,515)]), each=5),
-        rev(rainbow(55, start=0.13, end=0.23 )),
-        rev(rainbow(50, start=0.08, end=0.13 )[seq(1,50,length.out=15)]),
-        rev(rainbow(50, end=0.08)[seq(1,50,length.out=15)])), 
-        'brown2')
-    }
-      
-      
-    #SRCvalues <- c(-2,-1,0,1)
-    #if(sum(match(SRCvalues, unique(data.in))) == length(data.in)){ SRC plot}  
+
+    if(SRC){
+        color.system <- c("red", "lightgreen", "grey", "darkgreen") 
+        gg <- data.in + 3
+        title <- paste("SRC plot ", title, sep="")
+    } else {
+        if(color.gradient=='grey') {
+            color.system <- c()
+            for(i in seq(93,10,length.out=100)) color.system <- c(color.system, gray(i/100))
+            color.system <- c(gray(0.93), color.system, gray(0))
+        }
+        if(color.gradient=='blue') {
+            color.system <- c('grey88',
+            rainbow(45, start=0.5, end=0.65),                       
+            rainbow(10, start=0.65, end=0.7),
+            rainbow(45, start=0.7, end=0.85),
+            'red')
+        }
+        if(color.gradient=='red') {    
+            color.system <- c(
+            'grey88',
+            c(rep(c(colors()[c(417,417,515)]), each=5),
+            rev(rainbow(55, start=0.13, end=0.23 )),
+            rev(rainbow(50, start=0.08, end=0.13 )[seq(1,50,length.out=15)]),
+            rev(rainbow(50, end=0.08)[seq(1,50,length.out=15)])), 
+            'brown2')
+        } 
+        
+        #if range wanted is broader than possible, set to actual range limits
+        if(level.range[1]<min(data.in)) level.range[1] <- min(data.in)  
+        if(level.range[2]>max(data.in)) level.range[2] <- max(data.in)  
+        
+        #determine the color code to assess to each value  
+        g <- gg <- data.in
+        gg[gg <= level.range[1]]  <- level.range[1]
+        gg[gg >= level.range[2]] <- level.range[2]
+        gg <- gg-min(g) 
+        gg <- gg/max(gg)*100 + 1
+       
+        #over and under-ranged values set to limits of color range
+        gg[g < level.range[1]] <- 1
+        gg[g > level.range[2]] <- 102
+    }    
     
-    #if range wanted is broader than possible, set to actual range limits
-    if(level.range[1]<min(data.in)) level.range[1] <- min(data.in)  
-    if(level.range[2]>max(data.in)) level.range[2] <- max(data.in)  
-    
-    #determine the color code to assess to each value  
-    g <- gg <- data.in
-    gg[gg <= level.range[1]]  <- level.range[1]
-    gg[gg >= level.range[2]] <- level.range[2]
-    gg <- gg-min(g) 
-    gg <- gg/max(gg)*100 + 1
-   
-    #over and under-ranged values set to limits of color range
-    gg[g < level.range[1]] <- 1
-    gg[g > level.range[2]] <- 102
-    
-    
+        
     if(show.scale){
-        #setting the values written in legend
-        if(level.range[1] == min(data.in)) lmin <- round(level.range[1], digits=2) else lmin <- paste(round(level.range[1], digits=2), " or lower", sep="")
-        if(level.range[2] == max(data.in)) lmax <- round(level.range[2], digits=2) else {lmax <- paste(round(level.range[2], digits=2), " or over", sep="") ; color.system[102] <- "grey70"}
-    
         layout(matrix(c(1,2),nr=1), widths=c(5,1), heights=c(1,1))
         plot(XY[,2]~XY[,1], col=color.system[gg], cex=cex, pch=19, xlab='', ylab='', xaxt='n', yaxt='n', main=title)
         par(mar=c(0.1,0.1,0.1,0.1))
         plot(x=c(-1,1),y=c(0,1),xlim=c(0,1),ylim=c(0,1),type="n",axes=FALSE) 
         polygon(x=c(-2,-2,2,2),y=c(-2,2,2,-2),col="#f5fcba",border=NA)
-        legend(0.2,0.92,legend=list(lmax,'','','','',round((3*level.range[2]+level.range[1])/4, digits=2),'','','','',round(sum(level.range)/2, digits=2),
-         '','','','',round((level.range[2]+3*level.range[1])/4, digits=2),'','','','',lmin),cex=1, fill=rev(color.system[c(1,seq(2,101,length.out=19),102)]),bty='n')
+        
+        if(SRC){ legend(0,0.8,legend=list(' (1) new', ' (0) stable', '(-1) kept', '(-2) lost'),cex=1, fill=rev(color.system),bty='n') 
+         } else {
+          if(level.range[1] == min(data.in)) lmin <- round(level.range[1], digits=2) else lmin <- paste(round(level.range[1], digits=2), " or lower", sep="")
+          if(level.range[2] == max(data.in)) lmax <- round(level.range[2], digits=2) else {lmax <- paste(round(level.range[2], digits=2), " or over", sep="") ; color.system[102] <- "grey70"}
+
+          legend(0.2,0.92,legend=list(lmax,'','','','',round((3*level.range[2]+level.range[1])/4, digits=2),'','','','',round(sum(level.range)/2, digits=2),
+          '','','','',round((level.range[2]+3*level.range[1])/4, digits=2),'','','','',lmin),cex=1, fill=rev(color.system[c(1,seq(2,101,length.out=19),102)]),bty='n')
+        }
     }
      else plot(XY[,2]~XY[,1], col=color.system[gg], pch=19, xlab='', ylab='', xaxt='n', yaxt='n', main=title)  
+     
+     
 }
 
