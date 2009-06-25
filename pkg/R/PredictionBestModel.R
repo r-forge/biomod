5a3f648cddc80d99bc9424be646d21b4e7f41fbc
brunolafourcade (bruno lafourcade) <funkycastor@aol.com>
2009-06-25 13:16:27 +0000

    changed the format of the outputs, they are now not saved per species but condensed in arrays with D3=species
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biomod@85 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/PredictionBestModel.R
+++ pkg/R/PredictionBestModel.R
@@ -17,6 +17,10 @@ function(ANN=TRUE, CTA=TRUE, GAM=TRUE, GBM=TRUE,GLM=TRUE, MARS=TRUE, MDA=TRUE, R
         algo.c[names(which(!Biomod.material$algo.choice))] <- F  #switch off the models that are wanted but have not been trained    
     
         gg <- list()
+ 
+        #storing arrays of outputs
+        ARRAY.bin <- ARRAY.filt <- ARRAY <- array(NA, c(nrow(DataBIOMOD), max(Biomod.material$NbRun), Biomod.material$NbSpecies), dimnames=list(1:nrow(DataBIOMOD), rep(NA,max(Biomod.material$NbRun)), Biomod.material$species.names))
+    
     
         i <- 1
         while(i <= NbSp) {
@@ -32,13 +36,10 @@ function(ANN=TRUE, CTA=TRUE, GAM=TRUE, GBM=TRUE,GLM=TRUE, MARS=TRUE, MDA=TRUE, R
             G <- matrix(0, ncol=9, nrow=Biomod.material$NbRun[i], dimnames=list(1:Biomod.material$NbRun[i], c("GAM", "GLM", "GBM", "RF", "CTA", "MDA", "MARS", "ANN", "SRE")))            
             #storing info on best model for each species
             g <- as.data.frame(matrix(0, nrow=Biomod.material$NbRun[i], ncol=7, dimnames=list(1:Biomod.material$NbRun[i], c("Best.Model", 'Cross.validation','indepdt.data','total.score','Cutoff','Sensitivity','Specificity'))))
-            #storing matrices of outputs 
-            MAT.bin <- MAT.filt <- MAT <- matrix(NA, nr=dim(sp.data)[1], nc=Biomod.material$NbRun[i], dimnames=list(1:dim(sp.data)[1], rep(NA,Biomod.material$NbRun[i])))      
-    
+                 
     
             NbPA <- Biomod.material$NbRun[i] / (Biomod.material$NbRunEval+1)   #considering the number of PA runs that were done   
             nbrep <- Biomod.material$NbRunEval + 1
-    
        
             for(j in 1:NbPA){
                 for(k in 1:nbrep){ 
@@ -47,7 +48,7 @@ function(ANN=TRUE, CTA=TRUE, GAM=TRUE, GBM=TRUE,GLM=TRUE, MARS=TRUE, MDA=TRUE, R
                     if(Biomod.material$NbRepPA == 0) nam <- "full" else nam <- paste("PA", j, sep="")
                     if(k!=1) nam <- paste(nam, "_rep", k-1, sep="")
                     #assign name to column of matrix                    
-                    dimnames(MAT)[[2]][(j-1)*nbrep+k] <- nam 
+                    dimnames(ARRAY)[[2]][(j-1)*nbrep+k] <- nam 
                     rownames(g)[(j-1)*nbrep+k] <- nam   
                     nam <- paste(Biomod.material$species.names[i], nam, sep="_")
                     
@@ -58,43 +59,53 @@ function(ANN=TRUE, CTA=TRUE, GAM=TRUE, GBM=TRUE,GLM=TRUE, MARS=TRUE, MDA=TRUE, R
                     for(a in Biomod.material$algo[algo.c]){
                          if(a == temp){
                               g[(j-1)*nbrep+k, 2:7] <- eval(parse(text=paste("Evaluation.results.", method, sep="")))[[nam]][a,1:6]
-                              MAT[,(j-1)*nbrep+k] <- sp.data[,a,k,j]
-                              if(Bin.trans)  MAT.bin[,(j-1)*nbrep+k] <- BinaryTransformation(MAT[,(j-1)*nbrep+k], g[(j-1)*nbrep+k,5])
-                              if(Filt.trans) MAT.filt[,(j-1)*nbrep+k] <- FilteringTransformation(MAT[,(j-1)*nbrep+k], g[(j-1)*nbrep+k,5])
+
+                              #get the best model and fill with NAs to have predictions of the same length across species
+                              sdata <- sp.data[,a,k,j]
+                              sdataNA <- c(sdata, rep(NA, nrow(DataBIOMOD) - length(sdata)))
+                              sdata.bin <- BinaryTransformation(sdata, g[(j-1)*nbrep+k,5])
+                              sdata.bin <- c(sdata.bin, rep(NA, nrow(DataBIOMOD) - length(sdata)))
+                              sdata.filt <- FilteringTransformation(sdata, g[(j-1)*nbrep+k,5])
+                              sdata.filt <- c(sdata.filt, rep(NA, nrow(DataBIOMOD) - length(sdata)))
+
+                              ARRAY[ ,(j-1)*nbrep+k, i] <- sdataNA
+                              if(Bin.trans)  ARRAY.bin[ ,(j-1)*nbrep+k, i] <- sdata.bin
+                              if(Filt.trans) ARRAY.filt[ ,(j-1)*nbrep+k, i] <- sdata.filt
+
                          }
                     }
                     
                 } #nbrep k loop
             } #NbPA j loop
           
-            colnames(MAT.bin) <- colnames(MAT.filt) <- colnames(MAT)     
+            dimnames(ARRAY.bin)[[2]] <- dimnames(ARRAY.filt)[[2]] <- dimnames(ARRAY)[[2]]     
             g[,1] <- factor(max.col(G), levels=seq(along=colnames(G)), labels=colnames(G))     
-            gg[[SpNames[i]]] <- g
-
-            #objects assignations and storage on hard disk           
-            assign(paste("PredBestModelBy",method, "_", SpNames[i],sep=""), as.data.frame(MAT))
-            eval(parse(text=paste("save(PredBestModelBy",method, "_", SpNames[i],", file='", getwd(), "/pred/PredBestModelBy",method, "_", SpNames[i],"')", sep="")))
-            write.table(MAT, file=paste(getwd(),"/pred/PredBestModelBy",method, "_", SpNames[i],".txt", sep=""), row.names=F) 
-            
-            if(Bin.trans) {
-            assign(paste("PredBestModelBy",method, "_", SpNames[i],"_Bin",sep=""), as.data.frame(MAT.bin))
-            eval(parse(text=paste("save(PredBestModelBy",method, "_", SpNames[i],"_Bin, file='", getwd(), "/pred/PredBestModelBy",method, "_", SpNames[i],"_Bin')", sep="")))
-            write.table(MAT.bin, file=paste(getwd(),"/pred/PredBestModelBy",method, "_", SpNames[i],"_Bin.txt", sep=""), row.names=F)
-            }
-            
-            if(Filt.trans) {
-            assign(paste("PredBestModelBy",method, "_", SpNames[i],"_Filt",sep=""), as.data.frame(MAT.filt))
-            eval(parse(text=paste("save(PredBestModelBy",method, "_", SpNames[i],"_Filt, file='", getwd(), "/pred/PredBestModelBy",method, "_", SpNames[i],"_Filt')", sep="")))
-            write.table(MAT.filt, file=paste(getwd(),"/pred/PredBestModelBy",method, "_", SpNames[i],"_Filt.txt", sep=""), row.names=F)
-            }                            
+            gg[[SpNames[i]]] <- g                         
                     
             i <- i + 1
         } #species i loop
     
+
+        #objects assignations and storage on hard disk           
+        assign(paste("PredBestModelBy",method,sep=""), ARRAY)
+        eval(parse(text=paste("save(PredBestModelBy",method,", file='", getwd(), "/pred/PredBestModelBy",method, "')", sep="")))
+        write.table(ARRAY, file=paste(getwd(),"/pred/PredBestModelBy",method, ".txt", sep=""), row.names=F) 
+        
+        if(Bin.trans) {
+        assign(paste("PredBestModelBy",method, "_Bin",sep=""), ARRAY.bin)
+        eval(parse(text=paste("save(PredBestModelBy",method, "_Bin, file='", getwd(), "/pred/PredBestModelBy",method, "_Bin')", sep="")))
+        write.table(ARRAY.bin, file=paste(getwd(),"/pred/PredBestModelBy",method, "_Bin.txt", sep=""), row.names=F)
+        }
+        
+        if(Filt.trans) {
+        assign(paste("PredBestModelBy",method, "_Filt",sep=""), ARRAY.filt)
+        eval(parse(text=paste("save(PredBestModelBy",method, "_Filt, file='", getwd(), "/pred/PredBestModelBy",method, "_Filt')", sep="")))
+        write.table(ARRAY.filt, file=paste(getwd(),"/pred/PredBestModelBy",method, "_Filt.txt", sep=""), row.names=F)
+        }
+    
         #saving the list of best models per method
         assign(paste("BestModelBy", method, sep=""), gg)
         eval(parse(text=paste("save(BestModelBy",method,", file='", getwd(), "/pred/BestModelBy",method,"')", sep="")))
-    
     
     }}
 }
