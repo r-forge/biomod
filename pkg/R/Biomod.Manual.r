23ee9e79210b1b9d9faf83154300a38b39b6d5b2
thuiller (Dr ) <wilfried.thuiller@ujf-grenoble.fr>
2009-09-04 06:13:21 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biomod@96 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/Biomod.Manual.r
@@ -0,0 +1,34 @@
+Biomod.Manual<-function(){
+
+    paths <- .find.package("BIOMOD")
+    paths <- paths[file_test("-d", file.path(paths, "doc"))]
+    vignettes <- lapply(paths, function(dir) {
+        tools::list_files_with_exts(file.path(dir, "doc"), exts=c("pdf"))
+    })
+    topic <- "Biomod Manual"
+    vignettes <- as.character(unlist(vignettes))
+        vidx <- (tools::file_path_sans_ext(basename(vignettes)) ==
+            topic)
+        if (any(vidx)) {
+            pdf <- sub("\\.[[:alpha:]]+$", ".pdf", vignettes)
+            pidx <- file_test("-f", pdf)
+            ok <- vidx & pidx
+            if (any(ok)) {
+                idx <- min(which(ok))
+                if (sum(ok) > 1) {
+                  warning(gettextf("vignette '%s' found more than once,\nusing the one found in '%s'",
+                    topic, dirname(pdf[idx])), call. = FALSE,
+                    domain = NA)
+                }
+                z <- list(file = vignettes[idx], pdf = pdf[idx])
+            }
+            else {
+                z <- list(file = vignettes[vidx][1L], pdf = character(0L))
+            }
+            z$topic <- topic
+            class(z) <- "vignette"
+            return(z)
+        }
+        else warning(gettextf("vignette '%s' *not* found", topic),
+            call. = FALSE, domain = NA)
+}
