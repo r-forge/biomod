3b31d79b310624f1a61b6bc2283bd3fa38107ac4
brunolafourcade (bruno lafourcade) <funkycastor@aol.com>
2010-01-21 12:41:29 +0000

    Simplified coding
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biomod@132 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Migration.R
+++ pkg/R/Migration.R
@@ -1,45 +1,30 @@
 `Migration` <-
-function(CurrentPred=NULL, FutureProj=NULL, X=NULL, Y=
-NULL, MaxMigr=NULL, Pred.Save=NULL)
+function(CurrentPred=NULL, FutureProj=NULL, X=NULL, Y=NULL, MaxMigr=NULL, Pred.Save=NULL)
 {
     #if(dim(CurrentPred)[1] != dim(FutureProj)[1])
-    #stop("\n\n Number of pixel should be the same in CurrentPred, FutureProj, X and Y"
-    # )
+    #stop("\n\n Number of pixel should be the same in CurrentPred, FutureProj, X and Y")
     #if(dim(CurrentPred)[2] != dim(FutureProj)[2])
-    # stop("\n\n Species number should be the same in CurrentPred and FutureProj"
-    # )
-    NrowCur <- nrow(CurrentPred)
-    NcolCur <- ncol(CurrentPred)
-    NmaxMigr <- length(MaxMigr)
-    PredMigr <- as.data.frame(matrix(0, nrow=NrowCur))
-    if(NmaxMigr == 1) {
-        for(j in 1:NcolCur)
-            if(sum(FutureProj[CurrentPred[, j] == 0, j]) != 0) {
-                PredMigr[, j] <- Dispersal.Limit(CurrentPred[
-                    , j], FutureProj[, j], X, Y, MaxMigr)
-            }
+    #stop("\n\n Species number should be the same in CurrentPred and FutureProj")
+    
+    PredMigr <- as.data.frame(matrix(0, nrow=nrow(CurrentPred)))    
+    
+    if(length(MaxMigr) == 1 | length(MaxMigr) == ncol(CurrentPred)){
+        if(length(MaxMigr) == 1) MaxMigr <- rep(MaxMigr, ncol(CurrentPred))  #duplicate to nb of columns extent
+    
+        for(j in 1:ncol(CurrentPred)){
+            cat(paste("col", j, "\n")) 
+            if(sum(FutureProj[CurrentPred[, j] == 0, j]) != 0) 
+            PredMigr[, j] <- Dispersal.Limit(CurrentPred[, j], FutureProj[, j], X, Y, MaxMigr[j])
             else PredMigr[, j] <- FutureProj[, j]
-    }
-    else {
-        if(NmaxMigr == NcolCur) {
-            for(j in 1:NcolCur) {
-                if(sum(FutureProj[CurrentPred[, j] == 0, j]) !=
-                    0) {
-                    PredMigr[, j] <- Dispersal.Limit(
-                        CurrentPred[, j], FutureProj[
-                        , j], X, Y, MaxMigr[j])
-                }
-                else PredMigr[, j] <- FutureProj[, j]
-            }
-        }
-        else stop("\n\nDispersal limit not executed. NbPixel should be either a number or a vector in the same order than species column of CurrentPred and FuturPred")
-    }
-    if(!is.null(Pred.Save)) {
+        }    
+    } else stop("\n Incorrect values in MaxMigr, does not match the number of columns of data input")
+       
+    #Saving the results; checking if object already created   
+    if(!is.null(Pred.Save)){
         if(exists(Pred.Save, where=1)) {
             New.Pred.Save <- unique(Pred.Save, pos=1)
             assign(New.Pred.Save, PredMigr, pos=1)
-            warning(paste("\n\nPrediction with dispersal saved in",
-                New.Pred.Save))
+            warning(paste("\n Prediction with dispersal saved in", New.Pred.Save, "\n -> object overwritten \n"))
         }
         else assign(Pred.Save, PredMigr, pos=1)
     }
