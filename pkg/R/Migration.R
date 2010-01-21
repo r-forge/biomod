838b0492c4fefc47ec53b5575709309c36dff24d
brunolafourcade (bruno lafourcade) <funkycastor@aol.com>
2010-01-21 12:57:12 +0000

    added error tracking for non-binary input datasets
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biomod@133 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Migration.R
+++ pkg/R/Migration.R
@@ -6,13 +6,14 @@ function(CurrentPred=NULL, FutureProj=NULL, X=NULL, Y=NULL, MaxMigr=NULL, Pred.S
     #if(dim(CurrentPred)[2] != dim(FutureProj)[2])
     #stop("\n\n Species number should be the same in CurrentPred and FutureProj")
     
+    if(sum(CurrentPred[,1] >1) >=1 | sum(FutureProj[,1] >1) >=1) stop("\n Data inputs should be binary")
+    
     PredMigr <- as.data.frame(matrix(0, nrow=nrow(CurrentPred)))    
     
     if(length(MaxMigr) == 1 | length(MaxMigr) == ncol(CurrentPred)){
         if(length(MaxMigr) == 1) MaxMigr <- rep(MaxMigr, ncol(CurrentPred))  #duplicate to nb of columns extent
     
         for(j in 1:ncol(CurrentPred)){
-            cat(paste("col", j, "\n")) 
             if(sum(FutureProj[CurrentPred[, j] == 0, j]) != 0) 
             PredMigr[, j] <- Dispersal.Limit(CurrentPred[, j], FutureProj[, j], X, Y, MaxMigr[j])
             else PredMigr[, j] <- FutureProj[, j]
