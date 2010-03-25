cb52cf0dcc93e51b808315bc5153028eb9871cb2
brunolafourcade (bruno lafourcade) <funkycastor@aol.com>
2010-03-25 14:48:49 +0000

    New nnet cross validation function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biomod@147 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/CVnnet.R
+++ pkg/R/CVnnet.R
@@ -1,13 +1,18 @@
-`CVnnet` <-
-function(formula, data, truth, size=c(round(0.75*Biomod.material[["NbVar"]]), 
-round(0.75*Biomod.material[["NbVar"]]), round(0.75*Biomod.material[["NbVar"]]), Biomod.material[["NbVar"]], Biomod.material[["NbVar"]], Biomod.material[["NbVar"]]), lambda=rep(
-    c(0.01, 0.05, 0.1), 2), nreps=1, nifold=3, ...)
-{
-    choice <- numeric(length(lambda))
-    ri <- sample(nifold, nrow(data), replace=T)
-    for(j in seq(along=lambda)) {
-        choice[j] <- CVnnet1(formula, data, truth, nreps=nreps,ri=ri, size=size[j], decay=lambda[j], ...)
-    }
-    cbind(size=size, decay=lambda, fit=choice)
-}
+CV.nnet = function(Input, Target, size=c(2,4,6, 8), decay=c(0.001, 0.01, 0.05, 0.1)){
 
+    Eval = data.frame(matrix(0, ncol=3, nrow=16, dimnames=list(NULL, c("Size", "Decay", "AUC"))))
+    Eval[,1] = rep(size,4)
+    Eval[,2] = rep(decay, each=4)
+    for(i in 1:5){
+        set.seed(555)
+        Samp = SampleMat2(Target, 0.5)
+        Eval[,3] = Eval[,3] + apply(Eval[,1:2], 1, Samp, Target, Input, FUN=function(x, Samp, Target, Input){
+        nn = nnet(Input[Samp$calibration,], Target[Samp$calibration], size = x[1], decay = x[2], maxit = 200, trace = F)
+        AUC = somers2(predict(nn, Input[Samp$evaluation,]), Target[Samp$evaluation])["C"]
+        return(AUC)
+    })
+    }
+    Eval[,3] = Eval[,3]/10
+    z =which.max(Eval[,3])
+    return(Eval[z, 1:2])
+}
