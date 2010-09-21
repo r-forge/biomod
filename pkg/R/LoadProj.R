550357874d349d79134631ba8fc099a8078fcd2b
brunolafourcade (bruno lafourcade) <funkycastor@aol.com>
2010-09-21 08:56:39 +0000

    Adapted to loading raster projections.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biomod@226 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/LoadProj.R
+++ pkg/R/LoadProj.R
@@ -1,5 +1,5 @@
 `LoadProj` <-
-function(Sp=1, Bin=FALSE, Filt=FALSE, which.pred="pred"){
+function(Sp=1, Bin=FALSE, Filt=FALSE, which.pred="pred", raster=FALSE){
 
     #check the projection names wanted
     check.names <- c()
@@ -17,7 +17,9 @@ function(Sp=1, Bin=FALSE, Filt=FALSE, which.pred="pred"){
     #Check dimension calls for species
     if(length(Sp) > (Biomod.material$NbSpecies) | Sp=='all') Sp <- 1:(Biomod.material$NbSpecies)
 
-     
+
+
+    if(!raster){ 
     #------- Looping the loading of all the pred selected -------   
     for(i in 1:length(which.pred)){
          
@@ -42,6 +44,31 @@ function(Sp=1, Bin=FALSE, Filt=FALSE, which.pred="pred"){
                            
         }
     }
-
+    } else{           #raster projections
+    
+     for(i in 1:length(which.pred)){
+         
+          pname <- paste("/proj.", which.pred[i], "/Proj_", which.pred[i], "_", sep="") 
+          MoD <- Biomod.material[[paste("proj.", which.pred, ".choice", sep="")]]
+       
+          for(m in Biomod.material$algo[MoD]){
+              for(k in Sp){
+              
+                  #load probabilities
+                  pred.name <- paste(getwd(), pname, Biomod.material$species.names[k], "_", m, ".raster", sep="") 
+                  if(file.exists(pred.name)) load(pred.name, envir=.GlobalEnv)
+   
+                  #load bin and filt
+                  if(Bin){ for(j in 1:3){ if(Biomod.material$evaluation.choice[j]){
+                      pred.name <- paste(getwd(), pname, Biomod.material$species.names[k], "_", m, "_Bin", names(Biomod.material$evaluation.choice)[j], ".raster", sep="") 
+                      if(file.exists(pred.name)) load(pred.name, envir=.GlobalEnv)
+                  }}}
+                  if(Filt){ for(j in 1:3){ if(Biomod.material$evaluation.choice[j]){
+                      pred.name <- paste(getwd(), pname, Biomod.material$species.names[k], "_", m, "_Filt", names(Biomod.material$evaluation.choice)[j], ".raster", sep="") 
+                      if(file.exists(pred.name)) load(pred.name, envir=.GlobalEnv)
+                  }}}
+                             
+              }
+          }
+     }}
 }
-
