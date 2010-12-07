b030dfe2edf4b6327943721646d8d2c767c4dafb
thuiller (Dr ) <wilfried.thuiller@ujf-grenoble.fr>
2010-12-07 17:05:15 +0000

    sre for raster - bug fixed
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biomod@253 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/sre.R
+++ pkg/R/sre.R
@@ -1,56 +1,56 @@
-`sre` <- function(Response=NULL, Explanatory=NULL, NewData=NULL, Quant=0.025, na.rm=TRUE)
+sre <- function (Response = NULL, Explanatory = NULL, NewData = NULL, 
+    Quant = 0.025) 
 {
-    if(Quant>=0.5 | Quant<0) stop("\n settings in Quant should be a value between 0 and 0.5 ")
-    quants <- c(0+Quant, 1-Quant)
-    
-    
-    #Check the variables from calib to predict
-    
-    
-    #Calibration on point data, projection on points or rasters
-    if(class(Explanatory)[1]!='RasterStack'){
-        Response <- as.data.frame(Response)                                                        #to get the colnames if only a vector is given
-        NbVar <- dim(Explanatory)[2]
-        #storing output
-        if(class(NewData)[1]!='RasterStack') Pred <- as.data.frame(matrix(0, nr=dim(NewData)[1], nc=dim(Response)[2], dimnames=list(seq(dim(NewData)[1]), colnames(Response))))
-        
-        #for multiple species(not active for rasters for now -> overwritting TF)
-        for(i in 1:dim(Response)[2]){
-            ref <- Explanatory[Response[,i]==1,]
-            
-            #object for storing the True/False values (now +1s)
-            if(class(NewData)[1]=='RasterStack') { TF <- NewData@layers[[1]] ; TF <- TF<TF@data@min[[1]] }                 #set raster to FALSE for all values (exept NAs)
-            else TF <- rep(0, dim(NewData)[1])
-            
-            
-            for(j in 1:NbVar){
-                Q <- quantile(ref[,j], probs=quants, na.rm=na.rm)
-                
-                if(class(NewData)[1]!='RasterStack'){
-                    TF <- TF + (NewData[,names(ref)[j]]>=Q[1])                                          #add the TRUE/FALSE values to vector (+T = +1)
-                    TF <- TF + (NewData[,names(ref)[j]]<=Q[2])
-                } else{                                                                             #NewData = raster
-                    TFmin <- NewData@layers[[which(NewData@layernames==names(ref)[j])]]>=Q[1]       #produce T/F rasterlayers
-                    TFmax <- NewData@layers[[which(NewData@layernames==names(ref)[j])]]<=Q[2]       
-                    TFmin["TRUE"] <- 1                                                              #convert them to 0s and 1s
-                    TFmax["TRUE"] <- 1
-                    TF <- TF + TFmin + TFmax                                                        #add to TF for storage accross variables
-                }      
-            }
-            TF[TF!=(NbVar*2)] <- 0                                                                  #convert to binary
-            TF[TF==(NbVar*2)] <- 1                                                                  #important to set to 0 first, then the 1s
-            if(class(TF)[1]!='RasterLayer') Pred[,i] <- TF else Pred <- TF                                          #store in matrix if point data
-        }
-    }
-    
-    #Calibration on RasterStack, projection on rasters  
-    #else{ 
-        
-    #    Q <- quantile(belalp.stk@layers[[1]], na.rm=TRUE, probs=quants)
-        
-    #}
-
-    
-    if(class(NewData)[1]!='RasterStack' & dim(Response)[2]==1) Pred <- Pred[[1]]   #return a vector, not a data frame
-    return(Pred)
+    if (Quant >= 0.5 | Quant < 0) 
+        stop("\n settings in Quant should be a value between 0 and 0.5 ")
+    
+    quants <- c(0 + Quant, 1 - Quant)
+    
+    if (class(Explanatory)[1] != "RasterStack") {
+        Response <- as.data.frame(Response)
+        NbVar <- ncol(Explanatory)
+        
+        if (class(NewData)[1] != "RasterStack") 
+            Pred <- as.data.frame(matrix(0, nr = dim(NewData)[1], 
+                nc = dim(Response)[2], dimnames = list(seq(dim(NewData)[1]), 
+                  colnames(Response))))
+                  
+        for (i in 1:dim(Response)[2]) {
+            ref <- Explanatory[Response[, i] == 1, ]
+            
+            if (class(NewData)[1] == "RasterStack") {
+                TF <- subset(NewData,1)
+                TF <- TF >= TF@data@min    ## create a mask with 1
+            
+            }else TF <- rep(0, dim(NewData)[1])
+            
+            for (j in 1:NbVar) {
+                capQ <- quantile(ref[, j], probs = quants)
+                
+                if (class(NewData)[1] != "RasterStack") {
+                  TF <- TF + (NewData[, names(ref)[j]] >= capQ[1])
+                  TF <- TF + (NewData[, names(ref)[j]] <= capQ[2])
+                }else {
+                  TFmin <- NewData@layers[[which(NewData@layernames == 
+                    names(ref)[j])]] >= capQ[1]
+                  TFmax <- NewData@layers[[which(NewData@layernames == 
+                    names(ref)[j])]] <= capQ[2]
+#                  TFmin["TRUE"] <- 1
+#                  TFmax["TRUE"] <- 1
+                  TF <- TF * TFmin * TFmax      ## modified
+                }
+            }
+            
+#            TF[TF != (NbVar * 2)] <- 0
+#            TF[TF == (NbVar * 2)] <- 1
+            
+            if (class(TF)[1] != "RasterLayer") 
+                Pred[, i] <- TF
+            else Pred <- TF
+        }
+    }
+    
+    if (class(NewData)[1] != "RasterStack" & ncol(Response) == 1) 
+        Pred <- Pred[[1]]
+    return(Pred)
 }
