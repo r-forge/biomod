b85373e26898c37499e628fb38d3c90285f2113a
thuiller (Dr ) <wilfried.thuiller@ujf-grenoble.fr>
2010-12-08 09:44:11 +0000

    sre for raster - bug fixed(again)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biomod@254 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/sre.R
+++ pkg/R/sre.R
@@ -1,56 +1,1 @@
-sre <- function (Response = NULL, Explanatory = NULL, NewData = NULL, 
-    Quant = 0.025) 
-{
-    if (Quant >= 0.5 | Quant < 0) 
-        stop("\n settings in Quant should be a value between 0 and 0.5 ")
-    
-    quants <- c(0 + Quant, 1 - Quant)
-    
-    if (class(Explanatory)[1] != "RasterStack") {
-        Response <- as.data.frame(Response)
-        NbVar <- ncol(Explanatory)
-        
-        if (class(NewData)[1] != "RasterStack") 
-            Pred <- as.data.frame(matrix(0, nr = dim(NewData)[1], 
-                nc = dim(Response)[2], dimnames = list(seq(dim(NewData)[1]), 
-                  colnames(Response))))
-                  
-        for (i in 1:dim(Response)[2]) {
-            ref <- Explanatory[Response[, i] == 1, ]
-            
-            if (class(NewData)[1] == "RasterStack") {
-                TF <- subset(NewData,1)
-                TF <- TF >= TF@data@min    ## create a mask with 1
-            
-            }else TF <- rep(0, dim(NewData)[1])
-            
-            for (j in 1:NbVar) {
-                capQ <- quantile(ref[, j], probs = quants)
-                
-                if (class(NewData)[1] != "RasterStack") {
-                  TF <- TF + (NewData[, names(ref)[j]] >= capQ[1])
-                  TF <- TF + (NewData[, names(ref)[j]] <= capQ[2])
-                }else {
-                  TFmin <- NewData@layers[[which(NewData@layernames == 
-                    names(ref)[j])]] >= capQ[1]
-                  TFmax <- NewData@layers[[which(NewData@layernames == 
-                    names(ref)[j])]] <= capQ[2]
-#                  TFmin["TRUE"] <- 1
-#                  TFmax["TRUE"] <- 1
-                  TF <- TF * TFmin * TFmax      ## modified
-                }
-            }
-            
-#            TF[TF != (NbVar * 2)] <- 0
-#            TF[TF == (NbVar * 2)] <- 1
-            
-            if (class(TF)[1] != "RasterLayer") 
-                Pred[, i] <- TF
-            else Pred <- TF
-        }
-    }
-    
-    if (class(NewData)[1] != "RasterStack" & ncol(Response) == 1) 
-        Pred <- Pred[[1]]
-    return(Pred)
-}
+sre <- function (Response = NULL, Explanatory = NULL, NewData = NULL, Quant = 0.025) {    if (Quant >= 0.5 | Quant < 0)         stop("\n settings in Quant should be a value between 0 and 0.5 ")     quants <- c(0 + Quant, 1 - Quant)    if (class(Explanatory)[1] != "RasterStack") {        Response <- as.data.frame(Response)        NbVar <- ncol(Explanatory)         if (class(NewData)[1] != "RasterStack")             Pred <- as.data.frame(matrix(0, nr = dim(NewData)[1],                 nc = dim(Response)[2], dimnames = list(seq(dim(NewData)[1]),                   colnames(Response))))                          for (i in 1:dim(Response)[2]) {            ref <- Explanatory[Response[, i] == 1, ]                        if (class(NewData)[1] == "RasterStack") {                TF <- subset(NewData,1)                TF <- TF >= TF@data@min    ## create a mask with 1                        }else TF <- rep(0, dim(NewData)[1])                        for (j in 1:NbVar) {                capQ <- quantile(ref[, j], probs = quants)                                if (class(NewData)[1] != "RasterStack") {                  TF <- TF + (NewData[, names(ref)[j]] >= capQ[1])                  TF <- TF + (NewData[, names(ref)[j]] <= capQ[2])                }else {                  TFmin <- NewData@layers[[which(NewData@layernames ==                     names(ref)[j])]] >= capQ[1]                  TFmax <- NewData@layers[[which(NewData@layernames ==                     names(ref)[j])]] <= capQ[2]#                  TFmin["TRUE"] <- 1#                  TFmax["TRUE"] <- 1                  TF <- TF * TFmin * TFmax      ## modified                }            }            #            TF[TF != (NbVar * 2)] <- 0#            TF[TF == (NbVar * 2)] <- 1                        if (class(TF)[1] != "RasterLayer")                 Pred[, i] <- TF            else Pred <- TF        }    }        if (class(NewData)[1] != "RasterStack" & ncol(Response) == 1)         Pred <- Pred[[1]]    return(Pred)}
