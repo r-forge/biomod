24b80fafadf167b954f7ffa9434138091fff0e39
dgeorges (damien georges) <damien.georges2@gmail.com>
2013-07-22 16:30:06 +0000

    remove rescaler prevalence
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biomod@602 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/biomod2/R/Biomod.Models_RE.R
+++ pkg/biomod2/R/Biomod.Models_RE.R
@@ -548,7 +548,8 @@
   # scale or not predictions =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= #
   if(scal.models & !inherits(g.pred,'try-error')){
     cat("\n\tModel scaling...")
-    model.bm@scaling_model <- try(.scaling_model(g.pred/1000, Data[, 1], prevalence=0.5))
+#     model.bm@scaling_model <- try(.scaling_model(g.pred/1000, Data[, 1], prevalence=0.5))
+    model.bm@scaling_model <- try(.scaling_model(g.pred/1000, Data[, 1], weights=Yweights))
     g.pred <- try(predict(model.bm, Data[,expl_var_names,drop=FALSE], on_0_1000=TRUE))
   }
   
