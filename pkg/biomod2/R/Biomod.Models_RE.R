49d717f19847b4fd638c8cd5208b59fef6ec96c6
dgeorges (damien georges) <damien.georges2@gmail.com>
2014-10-08 14:07:18 +0000

    re-enhance weights for scalling models to prevent from low prob due to prevalence
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biomod@673 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/biomod2/R/Biomod.Models_RE.R
+++ pkg/biomod2/R/Biomod.Models_RE.R
@@ -591,7 +591,8 @@ model.sp <- try( gam::step.gam(gamStart, .scope(Data[1:3,-c(1,ncol(Data))], "s",
   # scale or not predictions =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= #
   if(scal.models & !inherits(g.pred,'try-error')){
     cat("\n\tModel scaling...")
-    model.bm@scaling_model <- try(.scaling_model(g.pred/1000, Data[, 1]))
+    #     model.bm@scaling_model <- try(.scaling_model(g.pred/1000, Data[, 1])) ## without weigths
+    model.bm@scaling_model <- try( .scaling_model(g.pred/1000, Data[, 1], weights= Yweights )) ## with weights
     g.pred <- try(predict(model.bm, Data[,expl_var_names,drop=FALSE], on_0_1000=TRUE))
   }
   
