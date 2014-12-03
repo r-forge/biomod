aa681187f9c5cac4e4a2b9b6a5aa7fe29a7decc3
dgeorges (damien georges) <damien.georges2@gmail.com>
2014-12-03 19:13:25 +0000

    track windows compil error
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biomod@691 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/biomod2/R/BiomodClass.R
+++ pkg/biomod2/R/BiomodClass.R
@@ -210,12 +210,7 @@ setMethod('BIOMOD.formated.data', signature(sp='numeric', env='RasterStack' ),
           function(sp,env,xy=NULL,sp.name=NULL, eval.sp=NULL, eval.env=NULL, eval.xy=NULL, na.rm=TRUE){
             cat("\n*** BIOMOD.formated.data l211")
             
-#             categorial_var <- names(env)[raster::is.factor(env)]
-            categorial_var <- NULL
-            try(cat("\n*** raster::names(env) = ", raster::names(env)))
-            try( cat("\n*** names(env) = ", names(env)))
-try( cat("\n*** raster::is.factor(env) = ", raster::is.factor(env)))
-  
+            categorial_var <- names(env)[raster::is.factor(env)]  
             
             cat("\n*** BIOMOD.formated.data l215")
             # take the same eval environemental variables than calibrating ones 
